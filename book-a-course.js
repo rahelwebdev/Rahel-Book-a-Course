08d1e18851ba7de87580a438b2cb574b813ef05a
rahelwebdev <kamberrahel@gmail.com>
2022-04-03 14:31:31 +0200

    js validation

--- book-a-course.js
+++ book-a-course.js
@@ -1,8 +1,72 @@
-
+/*
+<!DOCTYPE html>
+<html>
+<head>
 <script>
-function onSubmit() {
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+</script>
+</head>
+<body>
+
+<h2>JavaScript Validation</h2>
+
+<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
+  Name: <input type="text" name="fname">
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
+*/
+
+function validateForm() {
+    let vorname = document.getElementById("fname").value;
+    if (!vorname) {
+        alert ("Bitte tragen Sie Ihren Vornamen ein!");
+        return false;
+    }
+
+
+    let nachname = document.getElementById("lastName").value;
+    if(!nachname) {
+        alert("Bitte tragen Sie Ihren Nachnamen ein!");
+        return false;
+    }
+
+    let telefon = document.getElementById("phone").value;
+    if(!telefon) {
+        alert("Bitte tragen Sie Ihre Telefonnummer ein!");
+        return false;
+    }
+
+    let email = document.getElementById("email").value;
+    if(!email) {
+        alert ("Bitte tragen Sie Ihre E-mail-Adresse ein!")
+        return false;
+    }
+
+    let kurse = document.getElementById("kurse").value;
+    if (!kurse) {
+        alert ("Bitte geben Sie einen Kurs an!")
+        return false;
+    }
+}
+
+
+
+
+ 
 
     /*name required - inside the same function*/
+    /*
+    <script>
+function onSubmit() { 
     let fname = document.getElementById("fname").value;
     
     if (!fname) {
@@ -18,8 +82,8 @@ function onSubmit() {
 
     sessionStorage.setItem("fname", fname);
 }
-</script>
-  /*  const firstname = sessionStorage.getItem("firstname");
+</script> 
+   const firstname = sessionStorage.getItem("firstname");
         const email = sessionStorage.getItem("email");
 document.getElementById("confirmation").innerHTML =
             `<h3>Thanks for registering!</h3><br>
